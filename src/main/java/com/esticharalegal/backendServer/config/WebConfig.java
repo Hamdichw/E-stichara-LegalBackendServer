11af7858007d6102ac91f9d0459482f4d4ff19ba
Hamdi Chtiwi <mr.hamdichtiwi@gmail.com>
2024-04-12 00:43:57 +0100

    fix  Lawyer gets

--- src/main/java/com/esticharalegal/backendServer/config/WebConfig.java
+++ src/main/java/com/esticharalegal/backendServer/config/WebConfig.java
@@ -20,7 +20,7 @@ public class WebConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOrigin("http://localhost:4200");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
