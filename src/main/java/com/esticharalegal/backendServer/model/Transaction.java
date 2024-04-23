1bc02bf953df186e03fe8d2eada0cfd2c75ed20c
Hamdi Chtiwi <mr.hamdichtiwi@gmail.com>
2024-04-23 09:24:42 +0100

    fix  Lawyer gets

--- src/main/java/com/esticharalegal/backendServer/model/Transaction.java
+++ src/main/java/com/esticharalegal/backendServer/model/Transaction.java
@@ -1,5 +1,6 @@
 package com.esticharalegal.backendServer.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -27,6 +28,8 @@ public class Transaction {
 
     @Column(name = "date")
     private Date date;
+
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "lawyer_id")
     private User lawyer;
