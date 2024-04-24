b99219a9a699eb872b3d953e47df9ea325480b83
Hamdi Chtiwi <mr.hamdichtiwi@gmail.com>
2024-04-24 10:26:17 +0100

    fix  Lawyer gets

--- src/main/java/com/esticharalegal/backendServer/config/WebConfig.java
+++ src/main/java/com/esticharalegal/backendServer/config/WebConfig.java
@@ -21,6 +21,7 @@ public class WebConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("https://taupe-chimera-e8d6d6.netlify.app");
+        config.addAllowedOrigin("http://localhost:4200");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
